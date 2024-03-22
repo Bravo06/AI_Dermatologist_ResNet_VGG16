9924845cd997b9471c150b9547886fc9122ee656
Adit <aditdesai2002@gmail.com>
2024-03-23 00:29:45 +0530

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.11.2
+
+RUN apt-get update && apt-get install -y python3-opencv
+
+WORKDIR /app
+COPY . /app
+
+RUN pip install --upgrade pip
+RUN pip install tensorflow==2.13.0
+RUN pip install flask==2.2.5
+RUN pip install numpy==1.24.2
+RUN pip install waitress==3.0.0
+RUN pip install opencv-python
+
+EXPOSE 5000
+
+CMD waitress-serve --listen=0.0.0.0:5000 app:app
---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, jsonify
+import numpy as np
+import tensorflow as tf
+import cv2
+
+app = Flask(__name__)
+
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+models = {
+    "model_from_scratch" : tf.keras.models.load_model("static/model_from_scratch.h5"),
+    "resnet_model" : tf.keras.models.load_model("static/resnet_model.h5"),
+    "vgg16_model" : tf.keras.models.load_model("static/vgg16_model.h5")
+}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def runNeuralNetwork(img, model):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (256, 256))
+
+    x_test = np.array([img])
+    x_test = x_test.astype('float32')
+    x_test = x_test / 255.0
+
+    pred = models[model].predict(x_test)[0][0]
+    return str(pred * 100)
+
+@app.route('/', methods=['GET'])
+def test():
+    return 'API working'
+
+@app.route('/infer', methods=['POST'])
+def classifyImage():
+    if request.method == 'POST':
+        resText = ''
+        model = request.form.get('model')
+        print(model)
+        if 'image' not in request.files or not model:
+            resText = "Error uploading image"
+        else:
+            file = request.files['image']
+            
+            if file and allowed_file(file.filename) and file.filename != '':
+                # Read image from memory
+                nparr = np.frombuffer(file.read(), np.uint8)
+                img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+                resText = runNeuralNetwork(img, model)
+            else:
+                resText = "Error uploading image"
+        
+        response = jsonify({'resText' : resText})
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
